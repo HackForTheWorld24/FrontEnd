80e7f7e9239bab0c0d15edcd6afe8d81730e815b
sjangala02 <sjangala@stu.naperville203.org>
2024-03-02 12:30:24 -0600

    random star locations

--- functions.js
+++ functions.js
@@ -1,3 +1,17 @@
-function generateRandomNumber() {
-    return Math.floor(Math.random() * (2560 - 1000 + 1)) + 1000;
+function randomizeStarLocations() {
+    var stars = document.getElementsByClassName('random-stars');
+    for (var i = 0; i < stars.length; i++) {
+        // Fade out the star
+        stars[i].style.opacity = 0;
+
+        // After the star has faded out, move it and fade it back in
+        setTimeout(function(star) {
+            var randomX = Math.floor(Math.random() * window.innerWidth);
+            var randomY = Math.floor(Math.random() * window.innerHeight);
+            star.style.left = randomX + 'px';
+            star.style.top = randomY + 'px';
+            star.style.opacity = 1;
+        }, 500, stars[i]); // 500 should be the same as your CSS transition duration
+    }
 }
+setInterval(randomizeStarLocations, 2000); // 2000 should be at least twice your CSS transition duration
